a6fb57165c132e61d18d7ec0ad1518b31c8b4d98
Maxim Lejaeghere <maxim.lejaeghere@contribute.be>
2020-10-29 09:12:13 +0100

    29/10

--- Les 9/oefening 4/main.js
+++ Les 9/oefening 4/main.js
@@ -1,11 +1,13 @@
+ (function() {
+ 'use strict';
  let cards = [0,0,0,0,0,0,0,0,0,0];
- let redcard = Math.floor(Math.random() * 11); 
+ let redcard = Math.floor(Math.random() * 10); 
  cards[redcard] = 1;
  let chosenCard = 0;
-
+let firstOrLastCard = prompt('(E)erste of (L)aatste kaart');
 
 while(chosenCard == 0){
-let firstOrLastCard = prompt('(E)erste of (L)aatste kaart');
+let firstOrLastCard = prompt('Zwarte kaart! (E)erste of (L)aatste kaart');
 
 if (firstOrLastCard == 'E'){
 chosenCard = cards.shift();	
@@ -16,3 +18,4 @@ chosenCard = cards.pop();
 };
 
 alert('je hebt verloren, ververs de browser');
+})();
