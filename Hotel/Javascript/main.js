062f46c6d8d095c35db7763d19b557771f774fb9
Maxim Lejaeghere <maxim.lejaeghere@contribute.be>
2020-11-07 14:28:08 +0100

    calender and availibility

--- Hotel/Javascript/main.js
+++ Hotel/Javascript/main.js
@@ -1,4 +1,19 @@
+/* eslint-disable no-unused-vars */
 (function() {
-  'use strickt';
+  'use strict';
 })();
 
+function initMap() {
+  // The location of Uluru
+  const uluru = {lat: 51.05460503758255, lng: 3.7251363781042324};
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
