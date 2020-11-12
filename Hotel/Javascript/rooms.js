3c24ddaf92091ba032fbfe31a75fa983de48d7df
Maxim Lejaeghere <maxim.lejaeghere@contribute.be>
2020-11-12 09:39:00 +0100

    roomsTypes available

--- Hotel/Javascript/rooms.js
+++ Hotel/Javascript/rooms.js
@@ -1,21 +1,79 @@
-/* eslint-disable prefer-const */
 (function() {
   'use strict';
-  let myIndex = 0;
-  carousel();
+  let rooms;
+  const roomTypesAvailible = [];
+  function getData() {
+    const beginDateInput = new Date(document.getElementById('beginDateInput').value);
+    const endDateInput = new Date(document.getElementById('endDateInput').value);
+    fetch('../JSON/rooms.json')
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+        // gebruik de data in de respons
+          rooms = data;
+          let roomType;
+          let beginBooked;
+          let endBooked;
+          let i;
+          let j;
+          for (i = 0; i < rooms.length; i++) {
+            for (j = 0; j < rooms[i].bookedFrom.length; j++) {
+              beginBooked = new Date(rooms[i].bookedFrom[j]);
+              endBooked = new Date(rooms[i].bookedTo[j]);
+              if (beginBooked <= beginDateInput &&
+                 beginDateInput <= endBooked) {
+                break;
+              } else if (beginBooked <= endDateInput &&
+                endDateInput <= endBooked) {
+                break;
+              } else if (beginDateInput <= beginBooked &&
+                 endDateInput >= endBooked) {
+                break;
+              } else {
+                roomType = rooms[i].typeRoom;
 
-  function carousel() {
-    let i;
-    let x = document.getElementsByClassName('mySlides');
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = 'none';
+                if (roomTypesAvailible.includes(roomType)) {
+                } else {
+                  roomTypesAvailible.push(roomType);
+                }
+                break;
+              }
+            }
+          }
+          availibility();
+          roomTypesAvailible.length = 0;
+        })
+        .catch((err) => {
+        // doe iets met de error
+          console.log(`De request faalde :${err}`);
+        });
+  }
+
+  document.getElementById('btnAvailibility').addEventListener('click', getData);
+
+  function availibility() {
+    if (roomTypesAvailible.includes('junior suite')) {
+      document.getElementById('availableJunior').style.display = 'block';
+      document.getElementById('notAvailableJunior').style.display = 'none';
+    } else {
+      document.getElementById('notAvailableJunior').style.display = 'block';
+      document.getElementById('availableJunior').style.display = 'none';
     }
-    myIndex++;
-    if (myIndex > x.length) {
-      myIndex = 1;
+
+    if (roomTypesAvailible.includes('Vital room')) {
+      document.getElementById('availableVital').style.display = 'block';
+      document.getElementById('notAvailableVital').style.display = 'none';
+    } else {
+      document.getElementById('notAvailableVital').style.display = 'block';
+      document.getElementById('availableVital').style.display = 'none';
     }
-    x[myIndex-1].style.display = 'block';
-    setTimeout(carousel, 10000); // Change image every 2 seconds
+    if (roomTypesAvailible.includes('vitality room deluxe')) {
+      document.getElementById('availableDeluxe').style.display = 'block';
+      document.getElementById('notAvailableDeluxe').style.display = 'none';
+    } else {
+      document.getElementById('notAvailableDeluxe').style.display = 'block';
+      document.getElementById('availableDeluxe').style.display = 'none';
+    }
   }
 })();
-
